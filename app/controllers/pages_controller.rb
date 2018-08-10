f8b6420cba7a4c1fb3c27400f88d1ddec7ca4c86
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2018-08-10 14:25:46 +0100

    current on call on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   def home
+    @teams_on_call = Team
+      .all
+      .flat_map(&:calendars)
+      .flat_map(&:person_day_events)
+      .select { |e| e.date == Date.today }
+      .group_by { |e| e.calendar.team }
   end
 
   def sign_out
