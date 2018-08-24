b73c2c91c183806581135aa223ce4e643688eeb1
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2018-08-24 16:10:53 +0100

    stop existing pre-commit containers and optionally run shellcheck

--- pre-commit.sh
+++ pre-commit.sh
@@ -14,12 +14,14 @@ command -v docker > /dev/null || (echo "docker not installed" && exit 1)
 
 DB_CONTAINER_NAME="who-is-on-call-pre-commit-postgres"
 
-docker run --name $DB_CONTAINER_NAME --rm -p 5432:5432 postgres >log/postgres.log 2>&1 &
-
 function notsuccess {
-    docker stop $DB_CONTAINER_NAME
+    docker stop $DB_CONTAINER_NAME >/dev/null 2>&1 || echo -n ''
 }
 trap notsuccess EXIT
+
+notsuccess
+docker run --name $DB_CONTAINER_NAME --rm -p 5432:5432 postgres >log/postgres.log 2>&1 &
+
 
 bundle check || bundle install
 npm install
@@ -28,7 +30,7 @@ bin/rake db:create db:migrate
 
 bundle exec rake
 
-bin/rails t
+command -v shellcheck >/dev/null 2>&1 && shellcheck ./*.sh || echo "no shellcheck install, skipping test"
 
 echo "pre-commit success!"
 
