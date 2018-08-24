5ac6d83a41cff00ddea92414bd4551f806fab6a9
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2018-08-24 11:43:43 +0100

    remove unused controller method in pages
    this functionality is now in the sessions controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,9 +7,4 @@ class PagesController < ApplicationController
       .select { |e| e.date == Date.today }
       .group_by { |e| e.calendar.team }
   end
-
-  def sign_out
-    session.clear
-    redirect_to root_path
-  end
 end
