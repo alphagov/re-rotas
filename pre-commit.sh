8609b8231c6e7b3bcd644ac53c5a4235fa95987c
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2018-08-24 16:36:52 +0100

    add emoji to pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,39 +1,42 @@
 #!/usr/bin/env bash
-
 set -eu
 
-export RAILS_ENV=test
-export DB_USER=postgres
+export RAILS_ENV='test'
+export DB_USER='postgres'
+DB_CONTAINER_NAME='who-is-on-call-pre-commit-postgres'
+
+echo '🕵️   checking your horrible code'
 
 function precommit_fail {
-    echo "pre-commit fail!"
+    echo '‼️   pre-commit fail'
 }
 trap precommit_fail ERR
 
-command -v docker > /dev/null || (echo "docker not installed" && exit 1)
-
-DB_CONTAINER_NAME="who-is-on-call-pre-commit-postgres"
+command -v docker > /dev/null || (echo '🐳   Docker not installed' && exit 1)
 
 function stop_database {
-    docker stop $DB_CONTAINER_NAME >/dev/null 2>&1 || echo -n ''
+    docker stop "$DB_CONTAINER_NAME" >/dev/null 2>&1 || echo -n ''
 }
 trap stop_database EXIT
 
 stop_database
-docker run --name $DB_CONTAINER_NAME --rm -p 5432:5432 postgres >log/postgres.log 2>&1 &
+echo "🛢️   starting database $DB_CONTAINER_NAME 🐘"
+docker run --name "$DB_CONTAINER_NAME" \
+           --rm \
+           -p 5432:5432 \
+           postgres >log/postgres.log 2>&1 &
 
 bundle check || bundle install
 npm install
 
-bin/rake db:create db:migrate
-
-bundle exec rake
+rake db:create db:migrate
+rake
 
 if command -v shellcheck >/dev/null 2>&1; then
  shellcheck ./*.sh
 else
- echo "no shellcheck install, skipping test"
+ echo "‼️    no shellcheck install, skipping test"
 fi
 
-echo "pre-commit success!"
+echo '✅   pre-commit success!'
 
