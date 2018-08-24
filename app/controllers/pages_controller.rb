aa391e1eeccabc6132850ad421062f517a0716e6
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2018-08-24 11:57:31 +0100

    Show all teams on home even when noone on call
    This is really confusing for people when they are searching for a team,
    has it been deleted? No, theere is just no one on call for the team. We
    now show a message saying there is no one on call

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def home
-    @teams_on_call = Team
+    teams_with_empty_events = Team.all.map { |t| [t, []] }.to_h
+    events_by_team = Team
       .all
       .flat_map(&:calendars)
       .flat_map(&:person_day_events)
       .select { |e| e.date == Date.today }
       .group_by { |e| e.calendar.team }
+
+    @teams_on_call = teams_with_empty_events.merge(events_by_team)
   end
 end
