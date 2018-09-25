ea48c29f50d07ee596d88e38a50080a99da63503
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2018-09-25 14:42:38 +0100

    remove unnecessary comment

--- test/integration/cookies_test.rb
+++ test/integration/cookies_test.rb
@@ -10,8 +10,6 @@ class CookiesTest < ActionDispatch::IntegrationTest
     assert_response :success
 
     assert_not_nil cookies[CookieNames::SESSION_COOKIE_NAME]
-    # ideally want to check that the correct amount of cookies are set, something like:
-    # assert_equal 1, response.headers["Set-Cookie"].length
 
     # need to use response.headers to verify that SameSite and expiry are set :/
     assert_match /^#{CookieNames::SESSION_COOKIE_NAME}=.+$/, response.headers["Set-Cookie"]
