8d7f187b99db00e05c82f2de70fe034a285e7dc7
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-07-31 23:07:06 +0100

    add model and validation for audit events
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

---
+++ app/models/audit_event.rb
@@ -0,0 +1,11 @@
+class AuditEvent < ApplicationRecord
+  validates :email, presence: true
+
+  validates_each :event do |record, attr, val|
+    if attr == :event && !val.is_a?(Hash) && !val.key?(:message)
+      record.errors.add(attr, 'must include a :message in the event')
+    end
+  end
+
+  serialize :event, Hash
+end
