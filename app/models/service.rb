2982f39c04ba88b298c2cc69e3684b28c2130a7e
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-06-29 14:42:13 +0100

    services: implement basic scoring for services
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,13 @@
 class Service < ApplicationRecord
   validates :name, presence: true
+
+  def score
+    max_score = 2
+    score = 0
+
+    score += 1 unless description.blank?
+    score += 1 unless documentation.blank?
+
+    (score / max_score) * 5.0
+  end
 end
--- test/models/service_test.rb
+++ test/models/service_test.rb
@@ -12,4 +12,24 @@ class ServiceTest < ActiveSupport::TestCase
   test 'service can have empty documentation' do
     assert Service.new(name: 'my-service', documentation: '').valid?
   end
+
+  test 'service without documentation or description receives a score of 0' do
+    s = Service.new(
+      name: 'my-sad-service',
+      description: '',
+      documentation: '',
+    )
+
+    assert_equal s.score, 0
+  end
+
+  test 'service with documentation and description receives a score of 5' do
+    s = Service.new(
+      name: 'my-happy-service',
+      description: 'my-happy-service is described',
+      documentation: 'my-happy-service is documented',
+    )
+
+    assert_equal s.score, 5
+  end
 end
