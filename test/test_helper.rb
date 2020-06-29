2b179ee4314480e41d7debe973a8d32d74963102
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-06-29 13:31:57 +0100

    rails: get tests to use async
    when using the :test job adapter the tests don't pass because the async
    job doesnt run
    
    we expect the jobs to be performed locally, so don't care about this
    "feature"
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,7 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  setup do
+    (ActiveJob::Base.descendants << ActiveJob::Base).each(&:disable_test_adapter)
+  end
 end
