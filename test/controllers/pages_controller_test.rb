20467654f1aabca57be1cfc6c016882ba40c70e2
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-07-06 17:28:32 +0100

    home: do not show teams who have no one on rota
    we don't want to show teams who don't have anyone on a rota, it is noise
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -7,16 +7,4 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get root_path
     assert_redirected_to controller: :sessions, action: :new
   end
-
-  test 'should show information about a team even if no one is on a rota, when signed in' do
-    create_test_session_with_fake_auth
-    create_team("Biscuits", "Ensures quality biscuit supply")
-    get root_path
-    assert_response :success
-    assert_select "h1", "Currently on rotas"
-    assert_select("a:match('href', ?)", %r{/teams/biscuits}) do |result|
-        assert_match /Biscuits/, result.text, "No biscuits found, check your cookies!"
-    end
-    assert_select "p.govuk-body", "There is currently no one on a rota for Biscuits"
-  end
 end
