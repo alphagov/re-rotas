204f4d58bd881e5aa62d30d48e8b79fb377a645c
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-10-01 19:00:07 +0100

    ci: fix string concat cop
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- config/application.rb
+++ config/application.rb
@@ -20,15 +20,15 @@ module GdsRotas
       "X-Frame-Options" => "DENY",
       "X-XSS-Protection" => "1; mode=block",
       "X-Content-Type-Options" => "nosniff",
-      "Content-Security-Policy" => "default-src 'self'; " +
-        "font-src 'self' data:; " +
-        "img-src 'self' data:; " +
+      "Content-Security-Policy" => "default-src 'self'; " \
+        "font-src 'self' data:; " \
+        "img-src 'self' data:; " \
         "object-src 'none'; " +
         # the script digests are for the inline script in govuk-frontend/template.njk
         # if the scripts in that file change, or more are added, use a command similar to
         # this to generate the digests:
         # `echo "'sha256-"$(echo -n "document.body.className = ((document.body.className) ? document.body.className + ' js-enabled' : 'js-enabled');" | openssl dgst -sha256 -binary | openssl enc -base64)"'"`
-        "script-src 'self' 'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU=' 'unsafe-inline'; " +
+        "script-src 'self' 'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU=' 'unsafe-inline'; " \
         "style-src 'self' 'unsafe-inline'",
     }
 
