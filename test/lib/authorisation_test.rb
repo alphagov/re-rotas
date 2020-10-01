29b4ce6db80623a07022454922f01592a27c1be3
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-10-01 16:22:36 +0100

    ci: fix Lint/ParenthesesAsGroupedExpression cop
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- test/lib/authorisation_test.rb
+++ test/lib/authorisation_test.rb
@@ -2,18 +2,18 @@ require "test_helper"
 
 class RotasAuthorisationTest < ActiveSupport::TestCase
   test "valid" do
-    assert Rotas::Authorisation.email_authorised? ("valid@digital.cabinet-office.gov.uk")
+    assert Rotas::Authorisation.email_authorised?("valid@digital.cabinet-office.gov.uk")
   end
 
   test "invalid gov" do
-    assert_not Rotas::Authorisation.email_authorised? ("invalid@culture.gov.uk")
+    assert_not Rotas::Authorisation.email_authorised?("invalid@culture.gov.uk")
   end
 
   test "invalid nongov" do
-    assert_not Rotas::Authorisation.email_authorised? ("someone@gmail.com")
+    assert_not Rotas::Authorisation.email_authorised?("someone@gmail.com")
   end
 
   test "invalid random" do
-    assert_not Rotas::Authorisation.email_authorised? ("asdf")
+    assert_not Rotas::Authorisation.email_authorised?("asdf")
   end
 end
